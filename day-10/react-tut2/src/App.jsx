78c3dd1382cb801757c6c88704f511bf76dfd6b6
harshit-codprog <harshit.codprog@gmail.com>
2023-06-17 09:36:03 +0530

    add card component code

---
+++ day-10/react-tut2/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+    return (
+        <>
+            <Card>
+                <h1>Card Heading</h1>
+                <p>card paragraph</p>
+                <button>Card Button</button>
+            </Card>
+        </>
+    );
+}
+
+export default App;
---
+++ day-10/react-tut2/src/Card.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+import "./Card.css";
+function Card({ children }) {
+    return <div className="card">{children}</div>;
+}
+
+export default Card;
