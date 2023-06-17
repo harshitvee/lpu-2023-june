d6ce923e7173c45ac1ae81825fddffc7eac880d1
harshit-codprog <harshit.codprog@gmail.com>
2023-06-17 11:39:21 +0530

    add useState code

--- day-10/react-tut2/src/App.jsx
+++ day-10/react-tut2/src/App.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/jsx-key */
 import "./App.css";
 import Card from "./Card";
+import { v4 as uuid } from "uuid";
 const todos = [
-    { id: 1, title: "play cricket", completed: true },
-    { id: 2, title: "teach student to react", completed: false },
-    { id: 3, title: "watch web series", completed: false },
+    { title: "play cricket", completed: true },
+    { title: "teach student to react", completed: false },
+    { title: "watch web series", completed: false },
 ];
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
             {/* use implicit return  */}
 
             {todos.map((todo) => (
-                <Card>
+                <Card key={uuid()}>
                     <h1>title: {todo.title}</h1>
                     <h2>
                         {todo.completed ? "completed" : "not completed yet"}
