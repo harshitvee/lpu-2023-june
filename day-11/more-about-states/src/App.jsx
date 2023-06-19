a39f81949f70f3c67ecb980d29c470c2834852a6
harshit-codprog <harshit.codprog@gmail.com>
2023-06-19 09:36:34 +0530

    make separate component for counter application

--- day-11/more-about-states/src/App.jsx
+++ day-11/more-about-states/src/App.jsx
@@ -1,36 +1,10 @@
-import { useState } from "react";
-
+import CounterApp from "./CounterApp";
 import "./App.css";
-
 function App() {
-    const [count, setCount] = useState(0);
-
-    function handleIncrease() {
-        // setCount(count + 1);
-        // callback way
-        // setCount((prevCount) => {
-        //     return prevCount + 1;
-        // });
-        // use implicit return
-        setCount((prevCount) => prevCount + 1);
-    }
-
-    function handleReset() {
-        setCount(0);
-    }
     return (
-        <div style={{ textAlign: "center" }}>
-            <h1>{count}</h1>
-            <button onClick={handleIncrease}>Increase</button>
-            <button onClick={handleReset}>Reset</button>
-            <button
-                onClick={() => {
-                    setCount((prevCount) => prevCount - 1);
-                }}
-            >
-                Decrease
-            </button>
-        </div>
+        <>
+            <CounterApp />
+        </>
     );
 }
 
---
+++ day-11/more-about-states/src/CounterApp.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function CounterApp() {
+    const [count, setCount] = useState(0);
+
+    function handleIncrease() {
+        // setCount(count + 1);
+        // callback way
+        // setCount((prevCount) => {
+        //     return prevCount + 1;
+        // });
+        // use implicit return
+        setCount((prevCount) => prevCount + 1);
+    }
+
+    function handleReset() {
+        setCount(0);
+    }
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>{count}</h1>
+            <button onClick={handleIncrease}>Increase</button>
+            <button onClick={handleReset}>Reset</button>
+            <button
+                onClick={() => {
+                    setCount((prevCount) => prevCount - 1);
+                }}
+            >
+                Decrease
+            </button>
+        </div>
+    );
+}
+
+export default CounterApp;
