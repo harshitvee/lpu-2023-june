861028ffa15011680aeaa5b5f329208874477702
harshit-codprog <harshit.codprog@gmail.com>
2023-07-03 09:37:11 +0530

    create shopping-cart and setup tailwind

---
+++ day-22/shopping-cart/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+// backend as a service
+// 2 options : both are free
+
+// 1. firebase (document database)
+// 2. supabase (postgres database)
+
+function App() {
+    return <h1 className="text-3xl font-bold underline">shopping cart</h1>;
+}
+
+export default App;
