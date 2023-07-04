6879cbbd1c949093ef029bee8947b7d44ad99db1
harshit-codprog <harshit.codprog@gmail.com>
2023-07-04 11:57:26 +0530

    fetch products from database

--- day-23/e-commerce/src/Pages/Home.jsx
+++ day-23/e-commerce/src/Pages/Home.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { supabase } from "../config/supabase";
 function Home() {
+    async function fetchProducts() {
+        const { data, error } = await supabase.from("products").select();
+        console.log(data);
+    }
+    useEffect(() => {
+        fetchProducts();
+    }, []);
     return <h1>home</h1>;
 }
 
